be3be37d55b2cde4270dfad00cb16d911e73c832
Francisco Moretti <franciscoemoretti@gmail.com>
2025-10-23 07:26:08 +0100

    Update SITE_URL to use VERCEL_PROJECT_PRODUCTION_URL for improved deployment configuration.

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -25,8 +25,8 @@ type RootLayoutProps = {
 
 import type { Metadata } from "next";
 
-const SITE_URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+const SITE_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : "http://localhost:3000";
 
 export const metadata: Metadata = {
